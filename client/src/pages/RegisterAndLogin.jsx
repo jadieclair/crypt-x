c9626e3083d953c7ee2ffc2e501d479ddcf89232
t-whelan <whelan.tionne@gmail.com>
2023-11-14 11:33:07 +0200

    password character warning
    password character warning fixed

--- client/src/pages/RegisterAndLogin.jsx
+++ client/src/pages/RegisterAndLogin.jsx
@@ -23,6 +23,12 @@ function RegisterAndLogin() {
       return;
     }
 
+    // Validate password length
+    if (password.length < 6) {
+      setErrorMessage("Password must be at least 6 characters long.");
+      return;
+    }
+
     try {
       if (isSignIn) {
         const userCredential = await signInWithEmailAndPassword(
@@ -67,37 +73,47 @@ function RegisterAndLogin() {
   return (
     <div className="form-container">
       <div className="form-content">
-      <div className="row">
-        <div
-          className={isSignIn ? "pointer" : "activeColor"}
-          onClick={() => {
-            setIsSignIn(false);
-            setErrorMessage(""); // Clear any previous error messages
-          }}
-        >
-          Sign Up
+        <div className="row">
+          <div
+            className={isSignIn ? "pointer" : "activeColor"}
+            onClick={() => {
+              setIsSignIn(false);
+              setErrorMessage(""); // Clear any previous error messages
+            }}
+          >
+            Sign Up
+          </div>
+          <div
+            className={isSignIn ? "activeColor" : "pointer"}
+            onClick={() => {
+              setIsSignIn(true);
+              setErrorMessage(""); // Clear any previous error messages
+            }}
+          >
+            Sign In
+          </div>
         </div>
-        <div
-          className={isSignIn ? "activeColor" : "pointer"}
-          onClick={() => {
-            setIsSignIn(true);
-            setErrorMessage(""); // Clear any previous error messages
-          }}
-        >
-          Sign In
-        </div>
-      </div>
-      <h1 className="signup">{isSignIn ? "Sign In" : "Sign Up"}</h1>
-      <form onSubmit={handleSubmit}>
-        <input  className="inputs" name="email" placeholder="Email" required />
-        <br />
-        <input  className="inputs" name="password" type="password" placeholder="Password" required />
-        <br />
-        <p className="resetpass"onClick={handleReset}>Forgot Password?</p>
-        <br />
-        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-        <button className="signupbtn" type="submit">{isSignIn ? "Sign In" : "Sign Up"}</button>
-      </form>
+        <h1 className="signup">{isSignIn ? "Sign In" : "Sign Up"}</h1>
+        <form onSubmit={handleSubmit}>
+          <input className="inputs" name="email" placeholder="Email" required />
+          <br />
+          <input
+            className="inputs"
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <br />
+          <p className="resetpass" onClick={handleReset}>
+            Forgot Password?
+          </p>
+          <br />
+          {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+          <button className="signupbtn" type="submit">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </button>
+        </form>
       </div>
     </div>
   );
